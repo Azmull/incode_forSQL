8087abb2c123b0549cce34a7be3002cd898286f6
Shime <s95187463@gmail.com>
2025-12-10 16:06:33 +0800

    刪掉其他檔案instnwnd.sql，縮小整包檔案

--- incode_forSQL/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace incode_forSQL.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
