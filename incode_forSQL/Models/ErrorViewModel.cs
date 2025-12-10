d6ee5a4d9673ed733d1cb97e9c1eb0992a4bff90
Shime <s95187463@gmail.com>
2025-12-11 07:18:33 +0800

    把檔案都加回來，新增評論的所有table rating

---
+++ incode_forSQL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace incode_forSQL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
